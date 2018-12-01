8984a40a88e6606547e5828f06ef3da812cbdf3a
Reid Wilbur <reid.wilbur@gmail.com>
2018-11-30 23:24:47 -0600

    Day 1 part 1 sln

--- src/main/java/com/wilb0t/aoc/Day1.java
+++ src/main/java/com/wilb0t/aoc/Day1.java
@@ -1,26 +1,10 @@
 package com.wilb0t.aoc;
 
+import java.util.List;
+
 public class Day1 {
 
-  int getSumNext(String digits) {
-    int sum = 0;
-    for(int i = 0; i < digits.length(); i++) {
-      if (digits.charAt(i) == digits.charAt((i + 1) % digits.length())) {
-        // utf8/ascii char 48 is digit 0
-        sum += digits.charAt(i) - 48;
-      }
-    }
-    return sum;
-  }
-
-  int getSumHalfWay(String digits) {
-    int sum = 0;
-    for(int i = 0; i < digits.length(); i++) {
-      if (digits.charAt(i) == digits.charAt((i + (digits.length()/2)) % digits.length())) {
-        // utf8/ascii char 48 is digit 0
-        sum += digits.charAt(i) - 48;
-      }
-    }
-    return sum;
+  int getFrequency(List<Integer> adjs) {
+    return adjs.stream().reduce(0, (o, n) -> o + n);
   }
 }
